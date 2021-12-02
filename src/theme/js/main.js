533d19849060a44e9831e9030994da6c3a00ce02
vitaliynickishin <nickishin34@gmail.com>
2021-12-02 02:38:03 +0200

    testiomonials in work, refactoring

--- src/theme/js/main.js
+++ src/theme/js/main.js
@@ -9,6 +9,7 @@ $(function() {
   addActive();
   showImg();
   initCaseBoxSlider();
+  initTestiomonialsSlider();
   
   // Modal
   let modalBtn = $('.openmodal'),
@@ -224,7 +225,7 @@ let showImg = () => {
   })
 }
 
-//sleder blockchain
+//sleder cases
 let initCaseBoxSlider = function () {
   jQuery('.case-box-slider').slick({
     slidesToShow: 1,
@@ -237,6 +238,82 @@ let initCaseBoxSlider = function () {
     cssEase: 'linear'
   });
 };
+
+//sleder testimonials
+let initTestiomonialsSlider = function () {
+  // $('.testimonials-avatars-slider').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.testimonials-lists-slider'
+  // });
+  // $('.testimonials-lists-slider').slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.testimonials-avatars-slider',
+  //   dots: false,
+  //   focusOnSelect: true
+  // });
+  $('.testimonials-lists-slider').slick({
+ 	slidesToShow: 2,
+ 	slidesToScroll: 1,
+ 	arrows: true,
+ 	fade: false,
+ 	adaptiveHeight: true,
+ 	infinite: true,
+	useTransform: true,
+ 	speed: 400,
+ 	cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+ });
+
+ $('.testimonials-avatars-slider')
+ 	.on('init', function(event, slick) {
+ 		$('.testimonials-avatars-slider .slick-slide.slick-current').addClass('is-active');
+ 	})
+ 	.slick({
+ 		slidesToShow: 7,
+ 		slidesToScroll: 7,
+ 		dots: false,
+ 		focusOnSelect: false,
+ 		infinite: false,
+ 		responsive: [{
+ 			breakpoint: 1024,
+ 			settings: {
+ 				slidesToShow: 5,
+ 				slidesToScroll: 5,
+ 			}
+ 		}, {
+ 			breakpoint: 640,
+ 			settings: {
+ 				slidesToShow: 4,
+ 				slidesToScroll: 4,
+			}
+ 		}, {
+ 			breakpoint: 420,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+		}
+ 		}]
+ 	});
+
+ $('.testimonials-lists-slider').on('afterChange', function(event, slick, currentSlide) {
+ 	$('.testimonials-avatars-slider').slick('slickGoTo', currentSlide);
+ 	var currrentNavSlideElem = '.testimonials-avatars-slider .slick-slide[data-slick-index="' + currentSlide + '"]';
+ 	$('.testimonials-avatars-slider .slick-slide.is-active').removeClass('is-active');
+ 	$(currrentNavSlideElem).addClass('is-active');
+ });
+
+ $('.testimonials-avatars-slider').on('click', '.slick-slide', function(event) {
+ 	event.preventDefault();
+ 	var goToSingleSlide = $(this).data('slick-index');
+
+ 	$('.testimonials-lists-slider').slick('slickGoTo', goToSingleSlide);
+ });
+};
+
+
 
 //list-logo-slider mobile
 $(window).on('load resize', function() {
